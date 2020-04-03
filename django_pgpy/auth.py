c6cb3e87a12896f55081e602aedcbf7ccedb999f
Felix Eichenberger <felix.eichenberger@gyrotools.com>
2020-04-03 08:41:48 +0200

    Add login action

--- django_pgpy/auth.py
+++ django_pgpy/auth.py
@@ -1,13 +1,33 @@
 from django.contrib.auth.backends import ModelBackend
 
+
+def create_identity(user, password, superuser_identities):
+    from django_pgpy.models import Identity
+
+    if user and user.is_authenticated and not Identity.objects.exists_for_user(user):
+        Identity.objects.create(user, password, list(superuser_identities))
+
+
+def add_superusers_to_identity(user, password, superuser_identities):
+    from django_pgpy.models import Identity
+
+    if user and user.is_authenticated:
+        identity = Identity.objects.get(user=user)
+
+        if list(identity.encrypters.all().order_by('user__id')) != list(superuser_identities.all().order_by('user__id')):
+            identity.add_restorers(password, list(superuser_identities.all().order_by('user__id')))
+
+
 class ModelUserWithIdentityBackend(ModelBackend):
 
     def authenticate(self, request, username=None, password=None, **kwargs):
         from django_pgpy.models import Identity
 
         user = super().authenticate(request, username, password, **kwargs)
-        if user and user.is_authenticated and not Identity.objects.exists_for_user(user):
-            Identity.objects.create(user, password)
+
+        superuser_identities = Identity.objects.filter(user__is_superuser=True).exclude(user=user)
+        create_identity(user, password, superuser_identities)
+
         return user
 
 
@@ -18,16 +38,20 @@ class ModelUserWithIdentityAndSuperuserRestorersBackend(ModelBackend):
 
         user = super().authenticate(request, username, password, **kwargs)
         superuser_identities = Identity.objects.filter(user__is_superuser=True).exclude(user=user)
-        if user and user.is_authenticated and not Identity.objects.exists_for_user(user):
-            Identity.objects.create(user, password, list(superuser_identities))
 
-        self._add_superusers_to_identities(user, password, superuser_identities)
+        create_identity(user, password, superuser_identities)
+        add_superusers_to_identity(user, password, superuser_identities)
+
         return user
 
-    def _add_superusers_to_identities(self, user, password, superuser_identities):
+
+class IdentityLoginAction:
+
+    def run(self, request, user, username, password=None, **kwargs):
         from django_pgpy.models import Identity
 
-        if user:
-            identity = Identity.objects.get(user=user)
-            if list(identity.encrypters.all().order_by('user__id')) != list(superuser_identities.all().order_by('user__id')):
-                identity.add_restorers(password, list(superuser_identities.all().order_by('user__id')))
+        if user and user.is_authenticated:
+            superuser_identities = Identity.objects.filter(user__is_superuser=True).exclude(user=user)
+
+            create_identity(user, password, superuser_identities)
+            add_superusers_to_identity(user, password, superuser_identities)
