f022c1369467383e00e97dbd886c23fb3e3b1c6f
Felix Eichenberger <felix.eichenberger@gyrotools.com>
2020-06-29 23:12:48 +0200

    Version 0.1.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-pgpy',
-      version='0.1.5',
+      version='0.1.6',
       description='OpenPGP encryption for django.',
       author='Felix Eichenberger',
       author_email='felix.eichenberger@gyrotools.com',
