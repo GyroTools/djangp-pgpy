1e8785a454b55fad7b96ef8a3986effc1e2a22bf
martin <martin.buehrer@gyrotools.com>
2025-02-06 20:54:17 +0100

    added test config

--- tests/test_app/settings.py
+++ tests/test_app/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,7 +84,7 @@ DATABASES = {
             'USER': 'agora',
             'PASSWORD': 'agora',
             'HOST': '127.0.0.1',
-            'PORT': '',
+            'PORT': '5439',
         }
 }
 
